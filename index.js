06348db4c1c050f8ccb88627d66e13ac123d449f
bsqu <briansquashic@gmail.com>
2019-05-28 10:27:30 -0400

    added injectJSBeforeChart prop, I needed to customize various chart types with manual canvas drawing, and this needed to happen before the chart is initialized. made chartConfiguration prop a string. I needed to make use of chart callback functions within the chartConfiguration prop, and I could not get them working any other way.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import React, {
 } from 'react';
 import {
 	StyleSheet,
-	Platform
+  Platform
 } from 'react-native';
 import PropTypes from 'prop-types';
 
@@ -19,56 +19,83 @@ var settingChartScript = `
 `;
 
 export default class Chart extends Component {
-	
+
 	static propTypes = {
 		/**
 		 * 图表配置参数，对应chart.js中初始化需要的参数
 		 * @type {[object]}
 		 */
-		chartConfiguration: PropTypes.object.isRequired,
-		defaultFontSize : PropTypes.number
+		chartConfiguration: PropTypes.string,
+		defaultFontSize : PropTypes.number,
+    injectJSBeforeChart: PropTypes.string,
 	}
+
+
 	constructor(props) {
 		super(props);
 	}
+
+
 	componentWillReceiveProps(nextProps) {
-		if( nextProps.chartConfiguration !== this.props.chartConfiguration
-			|| nextProps.defaultFontSize !== this.props.defaultFontSize ){
-			this.setChart(nextProps.chartConfiguration, nextProps.defaultFontSize );
+		if (nextProps.chartConfiguration !== this.props.chartConfiguration
+		|| nextProps.defaultFontSize !== this.props.defaultFontSize) {
+			this.setChart(nextProps);
 		}
-	}
-	setChart(chartConfiguration, defaultFontSize) {
-		if( !chartConfiguration || undefined == defaultFontSize || null == defaultFontSize ){
-			return ;
-		}
-		this.webview && this.webview.injectJavaScript( 
-			settingChartScript.replace('{CONFIG}', JSON.stringify( chartConfiguration ))
-				.replace('{DEFAULT_FONT_SIZE}', defaultFontSize )
-		);
 	}
 
+
+  getJSToInject(props) {
+    const chartConfiguration = props.chartConfiguration || this.props.chartConfiguration;
+    const defaultFontSize = props.defaultFontSize || this.props.defaultFontSize;
+    const injectJSBeforeChart = props.injectJSBeforeChart || this.props.injectJSBeforeChart;
+
+    let injectJS = '';
+
+    if (injectJSBeforeChart)
+      injectJS += props.injectJSBeforeChart;
+
+    injectJS += settingChartScript.replace('{CONFIG}', chartConfiguration).replace('{DEFAULT_FONT_SIZE}', defaultFontSize)
+
+    return injectJS;
+  }
+
+
+	setChart(nextProps) {
+		if (!nextProps.chartConfiguration || undefined == nextProps.defaultFontSize || null == nextProps.defaultFontSize)
+			return;
+
+		this.webview && this.webview.injectJavaScript(this.getJSToInject(nextProps));
+	}
+
+
 	render() {
-		const defaultFontSize = this.props.defaultFontSize ? this.props.defaultFontSize : 12;
-		return ( < WebView style={{ flex : 1 }}
-					originWhitelist={["*"]}
-					ref = {
-						ref => this.webview = ref
-					}
-					injectedJavaScript = {
-						settingChartScript.replace( '{CONFIG}', JSON.stringify( this.props.chartConfiguration ))
-							.replace('{DEFAULT_FONT_SIZE}', defaultFontSize )
-					}
-					source= {Platform.OS == 'ios' ? require('./dist/index.html') : {uri: "file:///android_asset/dist/index.html"}}
-					
-					
-					onError = {
-						(error) => {
-							console.log(error)
-						}
-					}
-					// scalesPageToFit false for IOS and true for Android
-                                        scalesPageToFit={Platform.OS === 'ios' ? false : true}
-				/>	
+    const scrollEnabled = this.props.scrollEnabled !== undefined ? this.props.scrollEnabled : true;
+    const scaleToFit = this.props.scaleToFit !== undefined ? this.props.scaleToFit : true;
+    const injectJS = this.getJSToInject(this.props);
+		return (
+      <WebView style={{ flex : 1 }}
+        useWebKit={true}
+  			originWhitelist={["*"]}
+  			ref = {
+  				ref => this.webview = ref
+  			}
+  			injectedJavaScript = {
+  			   injectJS
+  			}
+  			source= {Platform.OS == 'ios' ? require('./dist/index.html') : {uri: "file:///android_asset/index.html"}}
+
+
+  			onError = {
+  				(error) => {
+  					console.log(error)
+  				}
+  			}
+  			// scalesPageToFit false for IOS and true for Android
+        scalesPageToFit={Platform.OS === 'ios' ? undefined : scaleToFit}
+        scrollEnabled={ scrollEnabled }
+        bounces={false}
+      />
 		)
 	}
+
 }
